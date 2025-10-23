d9c395465535e0621d083cfffb76902e6399c46b
SergeHenrich <mindjosserge2@gmail.com>
2025-10-23 23:30:38 +0100

    Ajout de la page A propos

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
 document.getElementById('signUpButton').addEventListener('click', function(){
     window.location.href='contact.html'
 });
+
+function togglesharemenu(){
+    const menu = document.getElementById('shareMenu');
+    menu.style.display = menu.style.display === 'none' ? 'block' :'none';
+}
